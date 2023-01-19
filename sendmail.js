08ba443c246dae1896944e62fbc43fc853847890
a.o <arthur.ott91@web.de>
2023-01-19 13:13:07 +0100

    update

--- sendmail.js
+++ sendmail.js
@@ -1,13 +1,12 @@
 window.addEventListener("DOMContentLoaded", function () {
 
   var form = document.getElementById("my-form");
-  // var button = document.getElementById("my-form-button");
   var status = document.getElementById("status");
 
   function success() {
       form.reset();
       status.classList.add('success')
-      status.innerHTML = "Thanks!";
+      status.innerHTML = "Danke!";
   }
   function error() {
       status.classList.add('error')
