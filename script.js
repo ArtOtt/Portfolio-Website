73fd4c9b15f9246b15399c93e276cb994e9c97ed
a.o <arthur.ott91@web.de>
2023-04-04 17:22:07 +0200

    update

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const navbarOffsetTop = navbar.offsetTop;
 const sections = document.querySelectorAll("section");
 const navbarLinks = document.querySelectorAll(".navbar-link");
 const progress = document.querySelector(".progress-bars-wrapper");
-const progressBarPercents = [90, 85, 80, 70, 55, 80, 80];
+const progressBarPercents = [90, 85, 70, 70, 80, 80];
 
 
 window.addEventListener("scroll", () => {
